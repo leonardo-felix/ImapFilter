ba46b78e2199a3a6dca9582e513e83e6c7739049
Leonardo Pires Felix <leonardo@piresfelix.com>
2018-07-07 23:02:45 -0300

    Primeira versão

--- imapfilter/models.py
+++ imapfilter/models.py
@@ -19,6 +19,9 @@ class Regra(models.Model):
     pasta_pesquisar = models.CharField(max_length=250, default='INBOX')
     pasta_destino = models.CharField(max_length=250)
 
+    def __str__(self):
+        return "de {0} para {1}".format(self.pasta_pesquisar, self.pasta_destino)
+
 
 @admin.register(Regra)
 class RegraAdmin(admin.ModelAdmin):
@@ -39,6 +42,9 @@ class ItemRegra(models.Model):
     regra_id = models.ForeignKey(Regra, on_delete=models.CASCADE)
     validacao = models.CharField(max_length=4, choices=OPCOES_VALIDACAO)
     conteudo = models.CharField(max_length=250)
+
+    def __str__(self):
+        return "{0} *{1}*".format(self.validacao, self.conteudo)
 
 
 @admin.register(ItemRegra)
